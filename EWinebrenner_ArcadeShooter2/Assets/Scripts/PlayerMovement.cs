1ac72f390cd9e5669b6d80ae9688ba89e2b3f9c3
Eric Winebrenner <ewinebrenner@champlain.edu>
2025-01-21 17:20:14 -0500

    Added player movement w/ keyboard

--- EWinebrenner_ArcadeShooter2/Assets/Scripts/PlayerMovement.cs
+++ EWinebrenner_ArcadeShooter2/Assets/Scripts/PlayerMovement.cs
@@ -2,29 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private void Awake()
-    {
-        
-    }
-    private void OnEnable()
-    {
-        
-    }
-    private void OnDisable()
-    {
-        
-    }
-    // Start is called before the first frame update
+    //member variables
+    [SerializeField] float _speed = 10.0f;
+    Rigidbody2D _rb;
+    Vector2 _input;
     void Start()
     {
-        
+        Debug.Log("Hello world");
+        _rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shoot");
+        }
+        _input = new Vector2(Input.GetAxis("Horizontal"), 
+            Input.GetAxis("Vertical"));
+       // input = input.normalized; //SET LENGTH TO 1 
+        _input = Vector2.ClampMagnitude(_input, 1.0f); //SET LENGTH TO 1 if TOO BIG
+        /* Vector3 pos = transform.position;
+        pos += new Vector3(input.x, input.y, 0) * _speed * Time.deltaTime;
+        transform.position = pos; */
+    }
+
+    //PHYSICS GOES HERE
+    private void FixedUpdate()
+    {
+        _rb.velocity = _input * _speed;
     }
 }
